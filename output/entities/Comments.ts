31776e3790c8a5b10f25cc498f36eeeed1ef0699
Rahmat Alamsyah <57090921+mattgan31@users.noreply.github.com>
2023-08-22 20:59:54 +0700

    Update the schema make the entities is change, and then the code must be changed too. i updated the Services for posts and relationships

--- output/entities/Comments.ts
+++ output/entities/Comments.ts
@@ -2,40 +2,37 @@ import {
   Column,
   Entity,
   Index,
+  JoinColumn,
   ManyToOne,
   PrimaryGeneratedColumn,
-} from 'typeorm';
-import { Posts } from './Posts';
-import { Users } from './Users';
+} from "typeorm";
+import { Posts } from "./Posts";
+import { Users } from "./Users";
 
-@Index('comments_pkey', ['id'], { unique: true })
-@Entity('comments', { schema: 'public' })
+@Index("comments_pkey", ["id"], { unique: true })
+@Entity("comments", { schema: "public" })
 export class Comments {
-  @PrimaryGeneratedColumn({ type: 'integer', name: 'id' })
+  @PrimaryGeneratedColumn({ type: "integer", name: "id" })
   id: number;
 
-  @Column('integer', { name: 'userId', nullable: true })
-  userId: number | false;
-
-  @Column('integer', { name: 'postId', nullable: false })
-  postId: number | null;
-
-  @Column('character varying', {
-    name: 'description',
-    nullable: false,
+  @Column("character varying", {
+    name: "description",
+    nullable: true,
     length: 255,
   })
   description: string | null;
 
-  @Column('timestamp with time zone', { name: 'createdAt' })
+  @Column("timestamp with time zone", { name: "createdAt" })
   createdAt: Date;
 
-  @Column('timestamp with time zone', { name: 'updatedAt' })
+  @Column("timestamp with time zone", { name: "updatedAt" })
   updatedAt: Date;
 
-  @ManyToOne(() => Posts, (post) => post.comment)
+  @ManyToOne(() => Posts, (posts) => posts.comments)
+  @JoinColumn([{ name: "postId", referencedColumnName: "id" }])
   post: Posts;
 
-  @ManyToOne(() => Users, (user) => user.posts)
+  @ManyToOne(() => Users, (users) => users.comments)
+  @JoinColumn([{ name: "userId", referencedColumnName: "id" }])
   user: Users;
 }
--- output/entities/Users.ts
+++ output/entities/Users.ts
@@ -4,39 +4,46 @@ import {
   Index,
   OneToMany,
   PrimaryGeneratedColumn,
-} from 'typeorm';
-import { Posts } from './Posts';
-import { Comments } from './Comments';
+} from "typeorm";
+import { Comments } from "./Comments";
+import { Posts } from "./Posts";
+import { Relationships } from "./Relationships";
 
-@Index('users_pkey', ['id'], { unique: true })
-@Entity('users', { schema: 'public' })
+@Index("users_pkey", ["id"], { unique: true })
+@Entity("users", { schema: "public" })
 export class Users {
-  @PrimaryGeneratedColumn({ type: 'integer', name: 'id' })
+  @PrimaryGeneratedColumn({ type: "integer", name: "id" })
   id: number;
 
-  @Column('character varying', {
-    name: 'username',
+  @Column("character varying", {
+    name: "username",
     nullable: true,
     length: 255,
   })
   username: string | null;
 
-  @Column('character varying', {
-    name: 'password',
+  @Column("character varying", {
+    name: "password",
     nullable: true,
     length: 255,
   })
   password: string | null;
 
-  @Column('timestamp with time zone', { name: 'createdAt' })
+  @Column("timestamp with time zone", { name: "createdAt" })
   createdAt: Date;
 
-  @Column('timestamp with time zone', { name: 'updatedAt' })
+  @Column("timestamp with time zone", { name: "updatedAt" })
   updatedAt: Date;
+
+  @OneToMany(() => Comments, (comments) => comments.user)
+  comments: Comments[];
 
   @OneToMany(() => Posts, (posts) => posts.user)
   posts: Posts[];
 
-  @OneToMany(() => Comments, (comment) => comment.user)
-  comment: Comments;
+  @OneToMany(() => Relationships, (relationships) => relationships.follower)
+  relationships: Relationships[];
+
+  @OneToMany(() => Relationships, (relationships) => relationships.following)
+  relationships2: Relationships[];
 }
